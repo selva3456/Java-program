0f0180b580e58705a5a510691bfc4090dad0ff03
Sriselvasakthimaari <ndhurairaj1967@gmail.com>
2025-08-27 18:45:04 +0530

    Added program code in test1.java

---
+++ test1.java
@@ -0,0 +1,14 @@
+class  test1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Start");
+		System.out.println("\"java\"");
+		System.out.println("\t\"SQL\"");
+		System.out.println("\t\t\"Python\"");
+		System.out.println("\t\t\t\"Html\"");
+		System.out.print("End");
+				
+	}
+}
+
